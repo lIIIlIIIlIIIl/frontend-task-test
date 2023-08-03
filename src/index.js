87862c4700c481f74ac178fad0073f3bca7ecdab
ShinDongJae <sdhsdj6450@gmail.com>
2023-08-03 13:02:16 +0900

    feat: api context로 관심사 분리

--- src/index.js
+++ src/index.js
@@ -2,6 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import ApiProvider from "./context/APIContext";
+import HttpClient from "./service/HttpClient";
+import InfoService from "./service/InfoService";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+
+const httpClient = new HttpClient();
+const infoService = new InfoService(httpClient);
+
+root.render(
+  <ApiProvider infoService={infoService}>
+    <App />
+  </ApiProvider>
+);
